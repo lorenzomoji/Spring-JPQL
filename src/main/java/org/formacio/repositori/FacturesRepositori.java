dbc8b367c62698c39156c66b77cc629ace476660
unknown <mojima16@gmail.com>
2019-05-26 11:27:41 +0200

    ejercicio acabado

--- src/main/java/org/formacio/repositori/FacturesRepositori.java
+++ src/main/java/org/formacio/repositori/FacturesRepositori.java
@@ -1,5 +1,8 @@
 package org.formacio.repositori;
 
+
+import java.util.List;
+
 import org.formacio.domain.Factura;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -8,7 +11,8 @@ import org.springframework.stereotype.Repository;
 
 public interface FacturesRepositori extends CrudRepository<Factura, Long> {
 	
-	@Query("select sum(lin.total) from Factura factura where client.cli_nom =: ?'joan'")
-	public double totalClient(String client); 
+	@Query("select sum(linia.total) from Factura factura join factura.linies linia where factura.client.nom = 1")
+	public Number totalClient(String client); 	
 	
+	public List<Factura> findByClientNom(String nom);
 }
--- src/main/java/org/formacio/servei/FacturesService.java
+++ src/main/java/org/formacio/servei/FacturesService.java
@@ -1,9 +1,20 @@
 package org.formacio.servei;
 
+import java.util.Optional;
+
 import org.formacio.domain.Factura;
+import org.formacio.domain.LiniaFactura;
+import org.formacio.repositori.FacturesRepositori;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class FacturesService {
 
+	@Autowired
+	FacturesRepositori facturaRepo;
+	
+	@Autowired
+	FidalitzacioService fidalitzacioServei;
+	
 	
 	/*
 	 * Aquest metode ha de carregar la factura amb id idFactura i afegir una nova linia amb les dades
@@ -15,6 +26,26 @@ public class FacturesService {
 	 */
 	public Factura afegirProducte (long idFactura, String producte, int totalProducte) {
 		
-		return null;
+		Optional <Factura> factura = facturaRepo.findById(idFactura);
+		
+		if (factura.isPresent()) {
+			LiniaFactura linea = new LiniaFactura();
+			linea.setId(idFactura);
+			linea.setProducte(producte);
+			linea.setTotal(totalProducte);
+			factura.get().getLinies().add(linea);
+			facturaRepo.save(factura.get()); 
+			
+			notificaPremi(factura.get());
+		}
+		
+		return factura.get();
+	}
+	
+	public void notificaPremi(Factura factura){
+		final int NUM_LINIES_AMB_PREMI = 4;
+		if(factura.getLinies().size() >= NUM_LINIES_AMB_PREMI){
+			fidalitzacioServei.notificaRegal(factura.getClient().getEmail());
+		}
 	}
 }
